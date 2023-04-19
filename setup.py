5ca0980879cffea93ed34bb10dc77a7c80c232c0
ReneGumzej <82899981+ReneGumzej@users.noreply.github.com>
2023-04-19 14:09:00 +0200

    deleted email

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
 name='mlproject',
 version='0.0.1',
 author='Rene Gumzej',
-author_email='rene.gumzej@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
