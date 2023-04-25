6b8b20491852fc3fcacdc97f281f64fad87c04d4
ReneGumzej <82899981+ReneGumzej@users.noreply.github.com>
2023-04-25 16:14:06 +0200

    change on data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -71,5 +71,5 @@ if __name__ == "__main__":
     data_ingestion = DataIngestion()
     train_data, test_data = data_ingestion.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    #data_transformation = DataTransformation()
+    #data_transformation.initiate_data_transformation(train_data, test_data)
