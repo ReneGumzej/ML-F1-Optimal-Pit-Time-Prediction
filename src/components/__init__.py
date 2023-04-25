b64e7b5d11249310e495f39c5adbb6fde0b51eac
ReneGumzej <82899981+ReneGumzej@users.noreply.github.com>
2023-04-25 18:00:32 +0200

    chanded data path

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -3,4 +3,4 @@ from definitions import ROOT_PATH
 
 data_path = r"notebook\data\data.csv"
 
-ABS_DATA_PATH = os.path.join(ROOT_PATH,data_path)
+ABS_DATA_PATH = fr"{ROOT_PATH}\notebook\data\data.csv"
