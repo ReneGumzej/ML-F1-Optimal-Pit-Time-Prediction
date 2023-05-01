e6e340ade5db73f94f50ce531c5de144f22d9e63
ReneGumzej <82899981+ReneGumzej@users.noreply.github.com>
2023-05-01 22:20:55 +0200

    deleted logging from train loop

--- model_main.py
+++ model_main.py
@@ -6,10 +6,13 @@ from src.components.model_trainer import ModelTrainer
 if __name__ == "__main__":
     data_ingestion = DataIngestion()
     train_data, test_data = data_ingestion.initiate_data_ingestion()
-    
+
     data_transformer = DataTransformation()
-    X_train, X_test,y_train, y_test, _ = data_transformer.initiate_data_transformation(train_data, test_data)
-    
+    X_train, X_test, y_train, y_test, _ = data_transformer.initiate_data_transformation(
+        train_data, test_data)
+
     model_trainer = ModelTrainer()
-    model_performance = model_trainer.initiate_model_trainer(X_train, y_train, X_test, y_test)
-    
+    model_performance = model_trainer.initiate_model_trainer(
+        X_train, y_train, X_test, y_test)
+
+    print(model_performance)
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -7,80 +7,73 @@ from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 from src.logger import logging
 
 from src.utils import get_best_params
 from config.config import model_parameter
 
+
 @dataclass
 class ModelTrainerConfiguration:
     model_object_path = os.path.join("artifacts", "model.pkl")
-    
+
+
 class ModelTrainer:
     def __init__(self) -> None:
         self.model_config = ModelTrainerConfiguration()
-        self.parameter = model_parameter
         
-        
+
     def initiate_model_trainer(self, X_train, y_train, X_test, y_test):
-        
+
         logging.info("Starting model training")
-        
+
         try:
             trained_model = []
             reached_score = []
 
             models = {
-            "Decision Tree": DecisionTreeRegressor(),
-            "Random Forest": RandomForestRegressor(),
-            "Gradient Boosting": GradientBoostingRegressor(),
-            "Linear Regression": LinearRegression(),
-            "XGBRegressor": XGBRegressor(),
-            "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-            "AdaBoost Regressor": AdaBoostRegressor(),
-            "KNearest Regressor": KNeighborsRegressor()
+                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+                "KNearest Regressor": KNeighborsRegressor()
             }
-            
+
             logging.info("Entering the train process")
-            
+
             for i in range(len(models)):
-                
+
                 model = list(models.values())[i]
 
-                parameter_grid = self.parameter[list(models.keys())[i]]
-                
-                
-                logging.info(f"preparing Model: '{models.keys()[i]}' with Parameter {parameter_grid} for training")
-                
+                parameter_grid = model_parameter[list(models.keys())[i]]
+
                 best_parameter = get_best_params(X_train, y_train, model, parameter_grid)
-                
-                logging.info("Estimated best parameters")
-                
+
                 model.set_params(**best_parameter)
-                logging.info("passing the parameters to models")
-                
-                logging.info("Start to train the Model")
+
                 model.fit(X_train, y_train)
-                
+
                 print(best_parameter)
-                
-                logging.info(f"Finished training for Model: {models.keys()[i]} ")
+
                 score = model.score(X_test, y_test)
-                
-                logging.info("Finished computation of model score")
-                
+
+                print("Model: ", model)
+                print("Score: ", score)
+
                 trained_model.append(model)
                 reached_score.append(score)
-            
-            model_performance = list(zip(trained_model,reached_score))
-            
-            logging.info("Completed model training loop")
-            return model_performance
 
-            
+            model_performance = list(zip(trained_model, reached_score))
+
+            logging.info("Completed model training loop")
+            return model_performance 
+
         except Exception as e:
-            CustomException(e,sys)
+            CustomException(e, sys)
